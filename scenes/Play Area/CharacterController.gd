34efc3ce0a3236b96211d01185456456e83f98e1
Wilyanse <willjose51@gmail.com>
2023-03-17 12:56:47 +0800

    Added comments to existing code

--- scenes/Play Area/CharacterController.gd
+++ scenes/Play Area/CharacterController.gd
@@ -1,16 +1,14 @@
 extends Area2D
 
 @export var speed = 400 # How fast the player will move (pixels/sec).
-var screen_size # Size of the game window.
-
-# Called when the node enters the scene tree for the first time.
+	
 func _ready():
-	screen_size = get_viewport_rect().size
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
+	# movement controls
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -20,11 +18,13 @@ func _process(delta):
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 
+	# velocity is used to implement diagonal movement
+	# also covers the case where two keys towards the opposite direction are pressed at the same time
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 	position += velocity * delta
 	
 
-
+# on collision function
 func _on_body_entered(body):
 	hide()
--- scenes/Play Area/PlayArea.gd
+++ scenes/Play Area/PlayArea.gd
@@ -11,47 +11,79 @@ var score
 var pause_scene = load("res://scenes/Play Area/PauseUI.tscn")
 
 # Called when the node enters the scene tree for the first time.
+# calls new game function to start the timers for the game
 func _ready():
 	new_game()
 
+# called when game over state is reached
+# unused
 func game_over():
 	$ScoreTimer.stop()
-	$MobTimer.stop()
+	$ProjectileTimer.stop()
 	$ObjectTimer.stop()
 
+# new game function
+# sets score to 0 and starts the start timer
+# delay is given so that the player is able to settle into the game before projectiles spawn
 func new_game():
 	score = 0
 	$StartTimer.start()
 
+
+# called when start timer timeouts
+# starts other timers
 func _on_start_timer_timeout():
-	$MobTimer.start()
+	$ProjectileTimer.start()
 	$ObjectTimer.start()
 	$ScoreTimer.start()
 
-
+# score is increased based on how long the player has survived
 func _on_score_timer_timeout():
 	score += 1
 
-
-func _on_mob_timer_timeout():
+# spawns a projectile for every timeout
+func _on_projectile_timer_timeout():
+	# spawns projectile
 	var spawnedProjectile = projectile.instantiate()
+	
+	# set location of projectile based on path
 	var projectileSpawnLocation = get_node("Character/ProjectilePath/ProjectileSpawnLocation")
+	
+	# randomizes spawnlocation along path
 	projectileSpawnLocation.progress_ratio = randf()
+	
+	# rotates projectile perpendicular to path
 	var direction = projectileSpawnLocation.rotation + PI / 2
+	
+	# changes projectile position to randomly chosen location
 	spawnedProjectile.position = projectileSpawnLocation.position
+	
+	# randomly changes the direction of projectile
 	direction += randf_range(-PI / 4, PI / 4)
 	spawnedProjectile.rotation = direction
+	
+	# randomly sets projectile velocity
 	var velocity = Vector2(randf_range(300.0, 700.0), 0.0)
 	spawnedProjectile.linear_velocity = velocity.rotated(direction)
+	
+	# adds projectile to scene
 	add_child(spawnedProjectile)
 
-
+# spawns an object for every timeout
 func _on_object_timer_timeout():
-	randomize()
+	# array of objects to spawn
 	var objs = [food, coin, req]
+	
+	# randomly chooses object to spawn
 	var chosenObj = objs[randi()% objs.size()]
+	
+	# spawns object
 	var spawnedObj = chosenObj.instantiate()
+	
+	# randomizes object spawn location along object path
 	var objSpawnLocation = get_node("Character/ObjectPath/ObjectSpawnLocation")
 	objSpawnLocation.progress_ratio = randf()
 	spawnedObj.position = objSpawnLocation.position
+	
+	# adds object to scene
 	add_child(spawnedObj)
