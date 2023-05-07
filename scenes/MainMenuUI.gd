75666741e905e4ac9ea5332f0c7dbbc38d2f5198
Wilyanse <willjose51@gmail.com>
2023-05-08 07:08:07 +0800

    Changed settings values from Loading Screen UI

--- scenes/MainMenuUI.gd
+++ scenes/MainMenuUI.gd
@@ -1,11 +1,5 @@
 extends Control
 
-var masterVolumeBus = AudioServer.get_bus_index("Master")
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	AudioServer.set_bus_volume_db(masterVolumeBus, SingletonScript.playerData["controls"]["playerVolume"])
-
 func _on_goToPlay_button_down():
 	get_tree().change_scene_to_file("res://scenes/SelectDifficultyUI.tscn")
 	pass # Replace with function body.
--- scenes/OpenSessionUI.gd
+++ scenes/OpenSessionUI.gd
@@ -1,21 +1,52 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var masterVolumeBus = AudioServer.get_bus_index("Master")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	AudioServer.set_bus_volume_db(masterVolumeBus, SingletonScript.playerData["controls"]["playerVolume"])
+	SetHotkeys()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 func _input(event):
 	#print(event.as_text())
 	if event.is_pressed():
 		get_tree().change_scene_to_file("res://scenes/MainMenuUI.tscn")
 		pass
 	
+func SetHotkeys():
+	
+	var event = InputMap.action_get_events("move_up")[0]
+	if SingletonScript.playerData["controls"]["playerWalkUp"] == 0:
+		event.set_keycode(KEY_UP)
+	else:
+		event.set_keycode(KEY_W)
+	
+	event = InputMap.action_get_events("move_down")[0]
+	if SingletonScript.playerData["controls"]["playerWalkDown"] == 0:
+		event.set_keycode(KEY_DOWN)
+	else:
+		event.set_keycode(KEY_S)
+	
+	event = InputMap.action_get_events("move_left")[0]
+	if SingletonScript.playerData["controls"]["playerWalkLeft"] == 0:
+		event.set_keycode(KEY_LEFT)
+	else:
+		event.set_keycode(KEY_A)
+	
+	event = InputMap.action_get_events("move_right")[0]
+	if SingletonScript.playerData["controls"]["playerWalkRight"] == 0:
+		event.set_keycode(KEY_RIGHT)
+	else:
+		event.set_keycode(KEY_D)
+	
+	event = InputMap.action_get_events("sprint")[0]
+	if SingletonScript.playerData["controls"]["playerSprint"] == 0:
+		event.set_keycode(KEY_Z)
+	else:
+		event.set_keycode(KEY_J)
+	
+	event = InputMap.action_get_events("nap")[0]
+	if SingletonScript.playerData["controls"]["playerSprint"] == 0:
+		event.set_keycode(KEY_X)
+	else:
+		event.set_keycode(KEY_K)
