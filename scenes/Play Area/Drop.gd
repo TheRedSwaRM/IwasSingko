a2f070f01c45a732b632957a0af9ceaa0d72d8e4
Wilyanse <willjose51@gmail.com>
2023-06-02 16:30:47 +0800

    removed req deletion from Drop enemy

--- scenes/Play Area/Drop.gd
+++ scenes/Play Area/Drop.gd
@@ -23,8 +23,9 @@ func _process(delta):
 			self.linear_velocity = Vector2(0.0, 0.0)
 
 func _on_body_entered(body):
-	if body.is_in_group("req"):
-		body.queue_free()
+	pass
+	#if body.is_in_group("req"):
+	#	body.queue_free()
 
 
 func _on_existence_timer_timeout():
